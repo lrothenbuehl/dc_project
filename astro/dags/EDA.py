e5fbb4767bd21d9256aca5798b49f42c128df2ae
residual233 <residual233@proton.me>
2024-05-21 15:51:54 +0200

    update some stuff

--- astro/dags/EDA.py
+++ astro/dags/EDA.py
@@ -3,12 +3,12 @@ from airflow.decorators import dag, task
 from airflow.datasets import Dataset
 from airflow.providers.papermill.operators.papermill import PapermillOperator
 
-# Define the datasets
+# Define the dataset
 unified_db = Dataset('/mnt/02-Data_Curation/unified.db')
-report = Dataset('/mnt/03-Data_exploration/DataExploration-completed.ipynb')
+EDA_report = Dataset('/mnt/03-Data_exploration/DataExploration-executed.ipynb')
 
 @dag(
-    dag_id='exploratory_data_analysis',
+    dag_id='EDA',
     default_args={'retries': 0},
     schedule=[unified_db],  # This ensures the DAG runs when the dataset is modified
     start_date=datetime(2022, 10, 1),
@@ -17,16 +17,15 @@ report = Dataset('/mnt/03-Data_exploration/DataExploration-completed.ipynb')
 )
 def data_producer_dag():
     
-    @task(outlets=[report])
-    def calculate_descriptive_statistics():
+    @task(outlets=[EDA_report])
+    def run_EDA():
         return PapermillOperator(
-            task_id="calculate_descriptive_statistics",
+            task_id="run_EDA",
             input_nb="/mnt/03-Data_exploration/DataExploration.ipynb",
-            output_nb="/mnt/03-Data_exploration/DataExploration.ipynb",
+            output_nb="/mnt/03-Data_exploration/DataExploration-executed.ipynb",
             parameters={"execution_date": "{{ execution_date }}", "virtual_env": "True"},
         ).execute({})
     
-    calculate_descriptive_statistics()
-    
+    run_EDA()
 
 data_producer_dag = data_producer_dag()
